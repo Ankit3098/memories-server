b21f63cb706fc1af5cf56dde1b1c1000f776bb31
Ankit3098 <ankitpatel3098@gmail.com>
2020-11-10 10:14:26 +0530

    first commit

---
+++ controllers/post.js
@@ -0,0 +1,74 @@
+import mongoose from 'mongoose'
+import PostMessage from '../models/postMessage.js' 
+
+export const getPosts = async (req,res,next)=>{
+
+    try {
+        const postMessage = await PostMessage.find()
+
+        res.status(200).json(postMessage)
+    } catch (error) {
+        res.status(404).json({message:error.message})
+    }
+
+}
+
+export const getPost = async (req,res,next)=>{
+    const { id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+        res.status(200).json(post)
+    } catch (error) {
+        res.status(409).json({message:error.message})
+    }
+}
+
+export const createPost = async (req,res,next)=>{
+
+    const {title, message, selectedFile, creator, tags} = req.body;
+    const newPost = new PostMessage({title, message, selectedFile, creator, tags})
+
+    try {
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({message:error.message})
+    }
+
+}
+
+export const updatePost = async (req,res,next)=>{
+    const { id } = req.params
+    const {title, message, selectedFile, creator, tags} = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post found with id : ${id}`)
+    
+    const updatedPost = { title, message, selectedFile, creator, tags, _id:id};
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true })
+    res.json(updatedPost)
+
+}
+
+export const likePost = async (req,res,next) =>{
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) res.status(404).send(`No post found with id : ${id}`)
+    const post = await PostMessage.findById(id);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new : true })
+
+    res.json(updatePost)
+
+}
+
+export const deletePost = async (req,res,next)=>{
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) res.status(404).send(`No post found with id : ${id}`)
+
+    await PostMessage.findByIdAndRemove(id)
+
+    res.json({message: 'Post deleted successfully.'})
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import postRoters from './routes/post.js'
+
+
+const app = express()
+dotenv.config()
+
+app.use(bodyParser.json({limit:"30mb", extended:true }))
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true }))
+app.use(cors())
+
+app.use('/post',postRoters)
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        app.listen(PORT,()=>console.log('Server is up and running'));
+    }).catch(err=>console.log(err.message));
+
+mongoose.set('useFindAndModify', false)
